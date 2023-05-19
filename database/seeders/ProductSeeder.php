84a3e969b4229327a89779aac5af61d96eff59b3
Alan Sanchez <alaansannchezz@gmail.com>
2023-05-19 18:38:10 -0300

    Se añadieron imagenes falsas a los productos

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -20,7 +20,7 @@ class ProductSeeder extends Seeder
                 fn ($product) =>
                 Image::factory(3)
                     ->create([
-                        'url' => fake()->imageUrl($width = 640, $height = 480),
+                        'url' => 'tests/products/' . fake()->image('public/storage/tests/products/', 640, 480, null, false),
                         'imageable_id' => $product->id,
                         'imageable_type' => Product::class
                     ])
